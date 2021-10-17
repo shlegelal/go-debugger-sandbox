6cd0ad40c53b5853b2fec6ad159a7661a4c378e2
TimPushkin <pushkin.td@gmail.com>
2021-10-17 23:09:50 +0300

    Simplify crossDeadlock

--- main/crossDeadlock.go
+++ main/crossDeadlock.go
@@ -6,20 +6,12 @@ func main() {
 	ch2 := make(chan int)
 
 	go func() {
-		for {
-			select {
-			case num := <-ch1:
-				ch2 <- num
-			}
-		}
+		num := <-ch1
+		ch2 <- num
 	}()
 
-	for {
-		select {
-		case num := <-ch2:
-			ch1 <- num
-		}
-	}
+	num := <-ch2
+	ch1 <- num
 }
 
 /*
