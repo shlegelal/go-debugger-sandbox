bfd03ada70101b81fde2b1c238180cf06c3084ef
AShlegel <st087000@stugent.spbu.ru>
2021-10-21 14:48:06 +0300

    Add waitGroup to mutexNoUnlock

--- main/deadlock/mutexNoUnlock.go
+++ main/deadlock/mutexNoUnlock.go
@@ -18,19 +18,25 @@ func printer(str string) {
 		fmt.Printf("%c ", s)
 		time.Sleep(time.Millisecond * 300)
 	}
-	// mutex.Unlock()  // Shared data access ends, unlock
+	mutex.Unlock() // Shared data access ends, unlock
 }
 
-func person1() {
+func person1(group *sync.WaitGroup) {
+	//defer group.Done()
 	printer("hello")
 }
 
-func person2() {
+func person2(group *sync.WaitGroup) {
+	//defer group.Done()
 	printer("world")
 }
 
 func main() {
-	go person1()
-	go person2()
+	var wg sync.WaitGroup
+	//wg.Add(2)
+	go person1(&wg)
+	go person2(&wg)
 	time.Sleep(5 * time.Second)
+
+	//wg.Wait()
 }
