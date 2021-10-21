3ed01100fb032acd081fdb38ae3bcc7a161d50d8
AShlegel <st087000@stugent.spbu.ru>
2021-10-21 17:43:55 +0300

    Fix incrementRace

--- main/datarace/incrementRace.go
+++ main/datarace/incrementRace.go
@@ -8,21 +8,22 @@ import (
 var wg sync.WaitGroup
 
 func increment(counter *int, times int) {
+	defer wg.Done()
+
 	for i := 0; i < times; i++ {
 		*counter++
 	}
-
-	wg.Done()
 }
 
 func main() {
 	n := 0
+
 	wg.Add(2)
 
 	for i := 0; i < 2; i++ {
 		go increment(&n, 1000000)
 	}
 
-	// wg.Wait()
+	wg.Wait()
 	fmt.Println(n)
 }
